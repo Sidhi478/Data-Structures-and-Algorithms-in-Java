a8d34d043b03ad9c281171ee0f0f0469804e5566
Sidhi478 <dangwalsidhika23@gmail.com>
2025-08-29 18:36:26 +0530

    Update and rename variable.java to Variable.java

--- Lecture1/Variable.java
+++ Lecture1/Variable.java
@@ -1,12 +1,10 @@
 package Lecture1;
 
-public class variable {
-
+public class Variable {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int x = 9;
 		System.out.println(x);
-
 	}
-
 }
+
